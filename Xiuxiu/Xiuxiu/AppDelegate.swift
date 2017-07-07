b5c65aef919a601343b326658de83d448949a03b
张一奇 <zhangyiqi8876@163.com>
2017-07-07 17:45:55 +0800

    设置应用跳转逻辑

--- Xiuxiu/Xiuxiu/AppDelegate.swift
+++ Xiuxiu/Xiuxiu/AppDelegate.swift
@@ -44,6 +44,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    // MARK: 处理应用跳转
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        var op = [String: Any]()
+        
+        for opt in options {
+            
+            op[opt.key.rawValue] = opt.value
+        }
+        
+        if AlibcTradeSDK.sharedInstance().application(app, open: url, options: op) {
+            //处理其他app跳转到自己的app，如果百川处理过会返回YES
+        }
+        
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
